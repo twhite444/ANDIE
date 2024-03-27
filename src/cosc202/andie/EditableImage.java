0a4a7eaec8ea2f868119595af2e495808edff832
cooch498 <cooch498@student.otago.ac.nz>
2024-03-27 16:55:19 +1300

    Fixed weird Javadoc comment formatting

--- src/cosc202/andie/EditableImage.java
+++ src/cosc202/andie/EditableImage.java
@@ -11,27 +11,22 @@ import javax.imageio.*;
  * </p>
  * 
  * <p>
- * The EditableImage represents an image with a series of operations applied to
- * it.
+ * The EditableImage represents an image with a series of operations applied to it.
  * It is fairly core to the ANDIE program, being the central data structure.
- * The operations are applied to a copy of the original image so that they can
- * be undone.
- * THis is what is meant by "A Non-Destructive Image Editor" - you can always
+ * The operations are applied to a copy of the original image so that they can be undone.
+ * This is what is meant by "A Non-Destructive Image Editor" - you can always 
  * undo back to the original image.
  * </p>
  * 
  * <p>
- * Internally the EditableImage has two {@link BufferedImage}s - the original
- * image
+ * Internally the EditableImage has two {@link BufferedImage}s - the original image
  * and the result of applying the current set of operations to it.
  * The operations themselves are stored on a {@link Stack}, with a second
- * {@link Stack}
- * being used to allow undone operations to be redone.
+ * {@link Stack} being used to allow undone operations to be redone.
  * </p>
  * 
  * <p>
- * <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA
- * 4.0</a>
+ * <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a>
  * </p>
  * 
  * @author Steven Mills
@@ -41,9 +36,7 @@ class EditableImage {
 
     /** The original image. This should never be altered by ANDIE. */
     private BufferedImage original;
-    /**
-     * The current image, the result of applying {@link ops} to {@link original}.
-     */
+    /** The current image, the result of applying {@link ops} to {@link original}. */
     private BufferedImage current;
     /** The sequence of operations currently applied to the image. */
     private Stack<ImageOperation> ops;
@@ -91,30 +84,24 @@ class EditableImage {
      * 
      * <p>
      * Object instances in Java are accessed via references, which means that
-     * assignment does
-     * not copy an object, it merely makes another reference to the original.
-     * In order to make an independent copy, the {@code clone()} method is generally
-     * used.
-     * {@link BufferedImage} does not implement {@link Cloneable} interface, and so
-     * the
+     * assignment does not copy an object, it merely makes another reference to the original.
+     * In order to make an independent copy, the {@code clone()} method is generally used.
+     * {@link BufferedImage} does not implement {@link Cloneable} interface, and so the
      * {@code clone()} method is not accessible.
      * </p>
      * 
      * <p>
      * This method makes a cloned copy of a BufferedImage.
-     * This requires knowledge of some details about the internals of the
-     * BufferedImage,
+     * This requires knowledge of some details about the internals of the BufferedImage,
      * but essentially comes down to making a new BufferedImage made up of copies of
      * the internal parts of the input.
      * </p>
      * 
      * <p>
      * This code is taken from StackOverflow:
-     * <a href=
-     * "https://stackoverflow.com/a/3514297">https://stackoverflow.com/a/3514297</a>
+     * <a href="https://stackoverflow.com/a/3514297">https://stackoverflow.com/a/3514297</a>
      * in response to
-     * <a href=
-     * "https://stackoverflow.com/questions/3514158/how-do-you-clone-a-bufferedimage">https://stackoverflow.com/questions/3514158/how-do-you-clone-a-bufferedimage</a>.
+     * <a href="https://stackoverflow.com/questions/3514158/how-do-you-clone-a-bufferedimage">https://stackoverflow.com/questions/3514158/how-do-you-clone-a-bufferedimage</a>.
      * Code by Klark used under the CC BY-SA 2.5 license.
      * </p>
      * 
@@ -140,11 +127,9 @@ class EditableImage {
      * 
      * <p>
      * Opens an image from the specified file.
-     * Also tries to open a set of operations from the file with <code>.ops</code>
-     * added.
+     * Also tries to open a set of operations from the file with <code>.ops</code> added.
      * So if you open <code>some/path/to/image.png</code>, this method will also try
-     * to
-     * read the operations from <code>some/path/to/image.png.ops</code>.
+     * to read the operations from <code>some/path/to/image.png.ops</code>.
      * </p>
      * 
      * @param filePath The file to open the image from.
@@ -188,12 +173,10 @@ class EditableImage {
      * </p>
      * 
      * <p>
-     * Saves an image to the file it was opened from, or the most recent file saved
-     * as.
+     * Saves an image to the file it was opened from, or the most recent file saved as.
      * Also saves a set of operations from the file with <code>.ops</code> added.
      * So if you save to <code>some/path/to/image.png</code>, this method will also
-     * save
-     * the current operations to <code>some/path/to/image.png.ops</code>.
+     * save the current operations to <code>some/path/to/image.png.ops</code>.
      * </p>
      * 
      * @throws Exception If something goes wrong.
@@ -257,8 +240,7 @@ class EditableImage {
      * Saves an image to the file provided as a parameter.
      * Also saves a set of operations from the file with <code>.ops</code> added.
      * So if you save to <code>some/path/to/image.png</code>, this method will also
-     * save
-     * the current operations to <code>some/path/to/image.png.ops</code>.
+     * save the current operations to <code>some/path/to/image.png.ops</code>.
      * </p>
      * 
      * @param imageFilename The file location to save the image to.
@@ -306,8 +288,7 @@ class EditableImage {
      * Get the current image after the operations have been applied.
      * </p>
      * 
-     * @return The result of applying all of the current operations to the
-     *         {@link original} image.
+     * @return The result of applying all of the current operations to the {@link original} image.
      */
     public BufferedImage getCurrentImage() {
         return current;
@@ -323,8 +304,7 @@ class EditableImage {
      * method makes a fresh copy of the original and applies the operations to it in
      * sequence.
      * This is useful when undoing changes to the image, or in any other case where
-     * {@link current}
-     * cannot be easily incrementally updated.
+     * {@link current} cannot be easily incrementally updated.
      * </p>
      */
     private void refresh() {
