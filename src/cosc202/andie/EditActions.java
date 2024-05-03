220b95565b61a4b5cb88fb4b5b07093b873085f0
Liam Williamson <wilia181@student.otago.ac.nz>
2024-05-03 17:58:07 +1200

    implmented DrawRectangle and finished crop

--- src/cosc202/andie/EditActions.java
+++ src/cosc202/andie/EditActions.java
@@ -2,14 +2,7 @@ package cosc202.andie;
 
 import java.util.*;
 import java.awt.event.*;
-import java.awt.image.BufferedImage;
-import java.awt.AlphaComposite;
-import java.awt.Color;
 import java.awt.Cursor;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Toolkit;
-
 import javax.swing.*;
 
 /**
@@ -52,7 +45,8 @@ public class EditActions {
         actions = new ArrayList<Action>();
         actions.add(new UndoAction(bundle.getString("menu_edit_undo"), null, bundle.getString("menu_edit_undo"), Integer.valueOf(KeyEvent.VK_Z)));
         actions.add(new RedoAction(bundle.getString("menu_edit_redo"), null, bundle.getString("menu_edit_redo"), Integer.valueOf(KeyEvent.VK_Y)));
-        actions.add(new CropAction("crop or whatever", null, "lookit me!", null));
+        actions.add(new CropAction(bundle.getString("menu_edit_crop_image"), null, "Click and drag to crop image", null));
+        actions.add(new DrawRectangleAction(bundle.getString("menu_edit_drawRectangle"), null, bundle.getString("menu_edit_drawRectangle_desc"), null));
 
     }
 
@@ -173,6 +167,121 @@ public class EditActions {
 
         /**
      * <p>
+     * Action to draw a rectangle on an image.
+     * </p>
+     * 
+     * @see DrawRectangle
+     */
+    public class DrawRectangleAction extends ImageAction implements MouseListener, MouseMotionListener {
+
+        int rectStartX = 0;
+        int rectStartY = 0;
+        int rectWidth = 1;
+        int rectHeight = 1;
+
+        /**
+         * <p>
+         * Create a new DrawRectangleAction.
+         * </p>
+         * 
+         * @param name The name of the action (ignored if null).
+         * @param icon An icon to use to represent the action (ignored if null).
+         * @param desc A brief description of the action  (ignored if null).
+         * @param mnemonic A mnemonic key to use as a shortcut  (ignored if null).
+         */
+        DrawRectangleAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
+
+            super(name, icon, desc, mnemonic);
+
+        }
+
+        /**
+         * <p>
+         * Callback for when the DrawRectangleAction is triggered.
+         * </p>
+         * 
+         * <p>
+         * This method is called whenever the DrawRectangleAction is triggered.
+         * It draws a rectangle 
+         * </p>
+         * 
+         * @param e The event triggering this callback.
+         */
+        public void actionPerformed(ActionEvent e) {
+
+            target.addMouseListener(this);
+            target.addMouseMotionListener(this);
+
+            target.setCursor(new Cursor(Cursor.CROSSHAIR_CURSOR)); // changes the cursor to a cross
+
+        }
+
+        @Override
+        public void mousePressed(MouseEvent click) { // gets the position of the mouse when it is clicked
+
+            //System.out.println("clicky");
+
+            rectStartX = click.getX(); 
+            rectStartY = click.getY();
+
+            // draw a rectangle, this acts as the selection box
+            target.getImage().apply(new DrawRectangle(rectStartX, rectStartY, Math.abs(rectStartX - click.getX()), Math.abs(rectStartY - click.getY())));
+            target.repaint();
+            target.getParent().revalidate();
+
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent unclick) { // when the mouse is released
+
+            //System.out.println("un-clicky");
+
+            // remove the selection box
+            target.getImage().undo();
+
+            target.removeMouseListener(this); // removes the mouse listner
+            target.removeMouseMotionListener(this);
+
+            target.setCursor(new Cursor(Cursor.DEFAULT_CURSOR)); // returs the cursor to default
+
+            rectWidth = Math.abs(rectStartX - unclick.getX()); // gets the distance x & y between the click and the unclick
+            rectHeight = Math.abs(rectStartY - unclick.getY());
+
+            rectStartX = Math.min(rectStartX, unclick.getX()); // gets the most top left x, y corner of the selected rectangle
+            rectStartY = Math.min(rectStartY, unclick.getY());
+
+            target.getImage().apply(new DrawRectangle(rectStartX, rectStartY, rectWidth, rectHeight)); // draw the final rectangle
+            target.repaint();
+            target.getParent().revalidate();
+
+        }
+
+        @Override
+        public void mouseDragged(MouseEvent drag) { // whenever the mouse is dragged
+
+            target.getImage().undo(); // remove the preivious selection box and draw a new one
+            target.getImage().apply(new DrawRectangle(rectStartX, rectStartY, Math.abs(rectStartX - drag.getX()), Math.abs(rectStartY - drag.getY())));
+            target.repaint();
+            target.getParent().revalidate();
+
+        }
+
+        @Override
+        public void mouseMoved(MouseEvent arg0) {}
+
+        @Override
+        public void mouseClicked(MouseEvent arg0) {}
+
+        @Override
+        public void mouseEntered(MouseEvent arg0) {}
+
+        @Override
+        public void mouseExited(MouseEvent arg0) {}
+
+    }
+
+    /**
+     * <p>
      * Action to crop an image.
      * </p>
      * 
@@ -184,8 +293,6 @@ public class EditActions {
         int cropStartY = 0;
         int cropWidth = 1;
         int cropHeight = 1;
-
-        SelectionBoxPanel cropSelection = new SelectionBoxPanel(target);
 
         /**
          * <p>
@@ -232,12 +339,20 @@ public class EditActions {
             cropStartX = click.getX(); 
             cropStartY = click.getY();
 
+            // draw a rectangle, this acts as the selection box
+            target.getImage().apply(new DrawRectangle(cropStartX, cropStartY, Math.abs(cropStartX - click.getX()), Math.abs(cropStartY - click.getY())));
+            target.repaint();
+            target.getParent().revalidate();
+
         }
 
         @Override
         public void mouseReleased(MouseEvent unclick) {
 
             //System.out.println("un-clicky");
+
+            // remove the selection box
+            target.getImage().undo();
 
             target.removeMouseListener(this); // removes the mouse listner so crops dont keep happeneing
             target.removeMouseMotionListener(this);
@@ -257,9 +372,12 @@ public class EditActions {
         }
 
         @Override
-        public void mouseDragged(MouseEvent drag) { // whenever the mouse is clicked and moved
+        public void mouseDragged(MouseEvent drag) { // whenever the mouse is dragged
 
-            cropSelection.draw(cropStartX, cropStartY, drag.getX(), drag.getY());
+            target.getImage().undo(); // remove the preivious selection box and draw a new one
+            target.getImage().apply(new DrawRectangle(cropStartX, cropStartY, Math.abs(cropStartX - drag.getX()), Math.abs(cropStartY - drag.getY())));
+            target.repaint();
+            target.getParent().revalidate();
 
         }
 
@@ -274,46 +392,6 @@ public class EditActions {
 
         @Override
         public void mouseExited(MouseEvent arg0) {}
-
-    }
-
-    public class SelectionBoxPanel extends JPanel {// draws a selection box above the image
-
-        static BufferedImage selectionBoxImage;
-        static Graphics2D selectionBoxImageGraphics;
-        static ImageIcon selectionBoxIcon;
-        static JLabel selectionBoxLabel;
-        static JPanel SelectionBoxPanel;
-        static ImagePanel target;
-
-        public SelectionBoxPanel(ImagePanel target) { 
-
-            this.target = target;
-
-            selectionBoxImage = new BufferedImage(500, 500, BufferedImage.TYPE_INT_ARGB);
-            
-            selectionBoxImageGraphics = selectionBoxImage.createGraphics();
-
-            selectionBoxImageGraphics.setComposite(AlphaComposite.Clear);   // makes the image completley clear
-            selectionBoxImageGraphics.fillRect(0, 0, selectionBoxImage.getWidth(), selectionBoxImage.getHeight());
-
-            selectionBoxIcon = new ImageIcon(selectionBoxImage); // puts the image in an icon
-            selectionBoxLabel = new JLabel(selectionBoxIcon); // puts the icon in a label
-            SelectionBoxPanel = new JPanel();
-
-            SelectionBoxPanel.add(selectionBoxLabel);
-            target.add(SelectionBoxPanel); // adds the label to the panel
-
-        }
-
-        private static void draw( int x1, int y1, int x2, int y2) {
-
-            selectionBoxImageGraphics.setColor(Color.RED);
-            selectionBoxImageGraphics.fillRect(x1, y1, x2, y2);
-
-            System.out.println(x1 + " " + y1 + " " + x2 + " " + y2 + " " + selectionBoxImage.getWidth() + " " + selectionBoxImage.getHeight());
-
-        }
 
     }
 
